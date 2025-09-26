ba4c807eb060014d326a650e564a9eb6b9c96e65
herzen-alex <herzen.alex1@web.de>
2025-09-26 11:15:16 +0200

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-
+import Card from './components/Card.vue';
 </script>
 
 <template>
-
+    <div class="max-width">
+        <Card>
+            <h1>Übersicht</h1>
+            <p>Beispieltext</p>
+        </Card>
+    </div>
 </template>
 
 <style scoped>
-
+.max-width {
+    max-width: 1440px;
+    margin: auto;
+    padding: 0 16px;
+}
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Card'
+})
+</script>
+
+<style scoped>
+.card {
+ background: #023A6233;
+  border-radius: 16px;
+  padding: 16px;
+  color: #fff;
+  box-shadow: 0 4px 6px rgba(0,0,0,0.2);
+}
+</style>
